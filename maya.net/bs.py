53f1aea837ca76b0a80944356b9b35ba5be6ee0e
EDMIG <30895066+EDMIG@users.noreply.github.com>
2017-11-09 15:20:39 +0800

    update bs.py

--- maya.net/bs.py
+++ maya.net/bs.py
@@ -12,4 +12,30 @@ with open('f:/t7.txt','w') as t:
     w=0
     x.append(w)
     cmds.setAttr('Facial_Blends_nc11_2.Blink_Left',w)
-    
+    y=cmds.pointPosition('Body.vtx[6277]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[4131]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[4133]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[4143]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[3402]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[3496]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[3550]',w=1);x.append(y)
+    y=cmds.pointPosition('Body.vtx[3504]',w=1);x.append(y)
+
+    t.write(str(x)+'\n')
+
+    for i in range(1,LOOPS):
+
+        x=[]
+        w=random.random()
+        x.append(w)
+        cmds.setAttr('Facial_Blends_nc11_2.Blink_Left',w)
+        y=cmds.pointPosition('Body.vtx[6277]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[4131]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[4133]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[4143]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[3402]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[3496]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[3550]',w=1);x.append(y)
+        y=cmds.pointPosition('Body.vtx[3504]',w=1);x.append(y)
+
+        t.write(str(x)+'\n')
